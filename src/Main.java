b8c2e1c49346abcc75052bfe0c80482fa1bab9e5
Ashuhauhan <ashuc306@gmail.com>
2022-12-15 19:36:22 +0530

    Welcome program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Welcome to Line Comparision Program!");
+
+        System.out.println("Hello Welcome to Line Comparision Program.");
     }
 }
